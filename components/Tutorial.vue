aa50a656a34e7b00de946f2dbaef25246c8c1d7e
suainul2 <suainulovi@gmail.com>
2023-07-11 11:37:50 +0700

    ss

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -8,7 +8,7 @@
       </a>
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
         <h2 class="text-2xl leading-7 font-semibold">
-          Huha to your Nuxt Application huha hahi
+          Huha success to your Nuxt Application huha hahi
         </h2>
         <p class="mt-3 text-gray-600">
           We recommend you take a look at the <a href="https://nuxtjs.org" target="_blank" class="text-green-500 hover:underline">Nuxt documentation</a>, whether you are new or have previous experience with the framework.<br>
